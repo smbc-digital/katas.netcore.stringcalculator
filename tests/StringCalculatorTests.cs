d54c56e7a71df17ffb85b3f4a607037e1bd81af6
Colin Lees <colinleesa7x@gmail.com>
2021-05-26 09:48:49 +0100

    fix: clean up codebase and update to .NET 5

--- tests/StringCalculatorTests.cs
+++ tests/StringCalculatorTests.cs
@@ -22,57 +22,56 @@ namespace Katas.NetCore.StringCalculator.Tests
          *  - Throw a custom exception if the first 7 numbers match the Fibonacci Sequence
          *  
          *  DON'T WORRY if you can't complete all tests!
-         * 
          */
 
         [Fact]
         public void Calculate_EmptyString_ReturnsZero()
         {
-            StringCalculator calculator = new StringCalculator();
-            var result = calculator.Calculate("");
+            StringCalculator calculator = new();
+            int result = calculator.Calculate(string.Empty);
             Assert.Equal(0, result);
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_SingleNumber_ReturnsSameNumber()
         {
-            //e.g. "1" = 1
+            // e.g. "1" = 1
         }
 
         [Fact (Skip="complete previous test first")]
         public void Calculate_TwoNumbers_ReturnsSum()
         {
-            //e.g. "1,2" = 3
+            // e.g. "1,2" = 3
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_FiveNumbers_ReturnsSum()
         {
-            //e.g. "1,2,3,4,5" = 15
+            // e.g. "1,2,3,4,5" = 15
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_CanUseAlternativeDelimiters_ReturnsSum()
         {
-            //e.g. "1|2|3" = 6
+            // e.g. "1|2|3" = 6
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_NumbersLargerThan100_IgnoresOver100sInSum()
         {
-            //e.g. "1,2,100" = 3
+            // e.g. "1,2,100" = 3
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_NonNumericValue_ReturnsZero()
         {
-            //e.g. "1,Elephant,6" = 0
+            // e.g. "1,Elephant,6" = 0
         }
 
         [Fact(Skip="complete previous test first")]
         public void Calculate_FirstSevenInFibonacciSequence_ThrowsCustomFibonacciAlertException()
         {
-            //e.g. "0,1,1,2,3,5,8" = FibonacciAlertException!
+            // e.g. "0,1,1,2,3,5,8" = FibonacciAlertException!
         }
     }
 }
